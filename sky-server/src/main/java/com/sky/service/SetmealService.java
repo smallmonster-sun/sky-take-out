a064cdb723da6ae68615903a85b9ea6942510609
XiaoXiao <zhaoxuetong2023@outlook.com>
2026-05-22 21:18:50 +0800

    套餐的批量删除以及菜品删除的修正

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,37 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+/**
+ * @Project: sky-take-out
+ * @Package: com.sky.service
+ * @Author: SUN
+ * @Date: 2026/5/20 20:29
+ * @Version: v1.0.0
+ * @Description: 套餐管理
+ **/
+public interface SetmealService {
+	/**
+	 * 新增套餐，同时需要保存套餐和菜品的关联关系
+	 *
+	 * @param setmealDTO
+	 */
+	void saveWithDish(SetmealDTO setmealDTO);
+
+	/**
+	 * 分页查询
+	 * @param setmealPageQueryDTO
+	 * @return
+	 */
+	PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+	/**
+	 * 批量删除套餐
+	 * @param ids
+	 */
+	void deleteBatch(List<Long> ids);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,103 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.exception.DeletionNotAllowedException;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
+import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @Project: sky-take-out
+ * @Package: com.sky.service.impl
+ * @Author: SUN
+ * @Date: 2026/5/20 20:31
+ * @Version: v1.0.0
+ * @Description: 套餐管理业务实现
+ **/
+@Service
+@Slf4j
+public class SetmealServiceImpl implements SetmealService {
+
+	@Autowired
+	private SetmealMapper setmealMapper;
+	@Autowired
+	private SetmealDishMapper setmealDishMapper;
+	@Autowired
+	private DishMapper  dishMapper;
+
+
+	/**
+	 * 新增套餐，同时需要保存套餐和菜品的关联关系
+	 * @param setmealDTO
+	 */
+	@Transactional
+	public void saveWithDish(SetmealDTO setmealDTO) {
+		Setmeal setmeal = new Setmeal();
+		BeanUtils.copyProperties(setmealDTO, setmeal);
+
+		//向套餐插入数据
+		setmealMapper.insert(setmeal);
+		//获取生成的套餐id
+		Long setmealId = setmeal.getId();
+
+		List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+		setmealDishes.forEach(setmealDish -> {
+			setmealDish.setSetmealId(setmealId);
+		});
+		//保持套餐和菜品的关联关系
+		setmealDishMapper.insertBatch(setmealDishes);
+	}
+
+	/**
+	 * 分页查询
+	 * @param setmealPageQueryDTO
+	 * @return
+	 */
+	public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+		int pageNum = setmealPageQueryDTO.getPage();
+		int pageSize = setmealPageQueryDTO.getPageSize();
+
+		PageHelper.startPage(pageNum, pageSize);
+		Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+		return new PageResult(page.getTotal(),page.getResult());
+	}
+
+	/**
+	 * 批量删除套餐
+	 * @param ids
+	 */
+	@Transactional
+	public void deleteBatch(List<Long> ids) {
+		ids.forEach(id -> {
+			Setmeal setmeal = setmealMapper.getById(id);
+			if(StatusConstant.ENABLE == setmeal.getStatus()) {
+				throw new DeletionNotAllowedException(MessageConstant.SETMEAL_ON_SALE);
+			}
+		});
+		/*ids.forEach(setmealId -> {
+			//删除套餐中的数据
+			setmealMapper.deleteById(setmealId);
+			//删除套餐菜品关系表的数据
+			setmealDishMapper.deleteBySetmealId(setmealId);
+		});*/
+		setmealMapper.deleteByIds(ids);
+		setmealDishMapper.deleteBySetmealIds(ids);
+	}
+}
