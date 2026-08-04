26df008050ac27bb6716b63ea80e02387f7800b7
XiaoXiao <zhaoxuetong2023@outlook.com>
2026-08-04 22:36:01 +0800

    导出运营报表代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -51,4 +52,10 @@ public interface ReportService {
 	 * @return
 	 */
 	SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+	/**
+	 * 导出运用数据报表
+	 * @param response
+	 */
+	void exportBusinessData(HttpServletResponse response);
 }
